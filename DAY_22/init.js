0fdf509b4394d3b81de1130715a3bf20b0fcaf39
anuragbhati01 <anuragbhatiofficial@gmail.com>
2025-11-14 12:24:19 +0530

    Done for Day 22

---
+++ DAY_22/init.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat");
+
+main()
+  .then(() => {
+    console.log("Connections Done!");
+  })
+  .catch((err) => console.log(err));
+
+async function main(){
+   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+let allChats = [
+    {
+        from : "raj",
+        to : "ram",
+        msg: "happy birthday",
+        created_at : new Date()
+    },
+    {
+        from : "krish",
+        to : "harsh",
+        msg: "hi brother!",
+        created_at : new Date()
+    },
+    {
+        from : "amit",
+        to : "ranja",
+        msg: "congratulations on your birthday",
+        created_at : new Date()
+    },
+    {
+        from : "ashok",
+        to : "shivam",
+        msg: "give me my files",
+        created_at : new Date()
+    },
+    {
+        from : "gill",
+        to : "will",
+        msg: "we have same name",
+        created_at : new Date()
+    }
+]
+
+Chat.insertMany(allChats);
+
+
